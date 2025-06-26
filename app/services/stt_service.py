6d5f834922b7d6f89bd7b1549c2e9a059827464a
Ullas hm <141505985+Ullas-HM@users.noreply.github.com>
2025-06-26 12:33:21 +0530

    Create stt_service.py

---
+++ app/services/stt_service.py
@@ -0,0 +1,7 @@
+from deepgram import Deepgram
+
+def transcribe_audio(audio_bytes):
+    # Assume conversion to linear PCM already done
+    dg = Deepgram("DEEPGRAM_API_KEY")
+    response = dg.transcription.prerecorded({'buffer': audio_bytes, 'mimetype': 'audio/wav'})
+    return response['results']['channels'][0]['alternatives'][0]['transcript']
